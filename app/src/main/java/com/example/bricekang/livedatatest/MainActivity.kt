b23adca3f235bda25405f7a7c28b0e6f8054c154
brice <moonish1123@gmail.com>
2018-08-13 16:04:26 +0900

    viewModel Provider factory 추가.

--- app/src/main/java/com/example/bricekang/livedatatest/MainActivity.kt
+++ app/src/main/java/com/example/bricekang/livedatatest/MainActivity.kt
@@ -1,23 +1,35 @@
 package com.example.bricekang.livedatatest
 
+import android.app.Application
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.ref.WeakReference
 
 class MainActivity : AppCompatActivity() {
     private lateinit var model: NameViewModel
     private lateinit var view: NameView
+    private val viewFactory: ViewModelProvider.Factory = object: ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return if (modelClass.isAssignableFrom(NameViewModel::class.java!!)) {
+                NameViewModel(this@MainActivity) as T
+            } else {
+                modelClass.getConstructor(Application::class.java).newInstance(this@MainActivity)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        model = NameViewModel(this)
+        model = ViewModelProvider(this, viewFactory).get(NameViewModel::class.java)
         view = NameView(this, model)
 
         initUI()
@@ -59,7 +71,7 @@ class NameView(parent: AppCompatActivity, viewModel: NameViewModel) {
 
     private val nameChangeObserver = Observer<String> {
         it?.let {
-            refParent.get()?.textView?.text = it
+            textView.text = it
         }
     }
 
@@ -68,6 +80,7 @@ class NameView(parent: AppCompatActivity, viewModel: NameViewModel) {
             model.mCurrentName.observe(it, nameChangeObserver)
         }
 
+        textView.text = model.mCurrentName.value
     }
 
     fun deinit() {
